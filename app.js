b3fe452cac1450f59475727d0db040fbea2588b8
DemonDaddy22 <guptarohan369@gmail.com>
2019-09-17 16:15:18 +0530

    fix app.js

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 require('dotenv').config();
 
-var express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
-    flash           = require("connect-flash"),
-    cloudinary      = require("cloudinary"),
-    ejs             = require("ejs"),
-    multer          = require("multer"),
-    methodOverride  = require("method-override");
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	ejs = require('ejs'),
+	methodOverride = require('method-override');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 
-app.get('/', function(req, res){
-    res.render("home");
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
-app.get('*', function(req, res){
-    res.redirect("back");
+app.get('*', function(req, res) {
+	res.redirect('back');
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server started...");
+console.log(process.env.PORT);
+console.log(process.env.IP);
+app.listen(process.env.PORT, process.env.IP, function() {
+	console.log('> Server started');
 });
