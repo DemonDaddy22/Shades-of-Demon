74065da3df47f6b7cf8f8d1568eb6bc34d0a063f
DemonDaddy22 <guptarohan369@gmail.com>
2019-10-14 18:24:59 +0530

    restyle UI of homepage

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+// require('dotenv').config();
+
+// var express = require('express'),
+// 	app = express(),
+// 	bodyParser = require('body-parser'),
+// 	ejs = require('ejs'),
+// 	methodOverride = require('method-override');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/public'));
+// app.use(methodOverride('_method'));
+
+// app.get('/', function(req, res) {
+// 	res.render('home');
+// });
+
+// app.get('*', function(req, res) {
+// 	res.redirect('back');
+// });
+
+// console.log(process.env.PORT);
+// console.log(process.env.IP);
+// app.listen(process.env.PORT, process.env.IP, function() {
+// 	console.log('> Server started');
+// });
+
 require('dotenv').config();
 
-var express = require('express'),
+let express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	ejs = require('ejs'),
@@ -11,16 +38,19 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('*', function(req, res) {
+app.get('/home', (req, res) => {
+	res.redirect('/');
+});
+
+app.get('*', (req, res) => {
+	// CREATE AN ERROR PAGE TEMPLATE
 	res.redirect('back');
 });
 
-console.log(process.env.PORT);
-console.log(process.env.IP);
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, () => {
 	console.log('> Server started');
 });
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,59 @@
-var span = document.getElementById("intro-cap");
-var i = 0;
-setInterval(textChanger, 2000);
+jQuery(document).ready(function($){
 
-function textChanger(){
-    var texts = ['Developer', 'Foodie', "Photographer", "Person"];
-    span.innerHTML = texts[i];
-    i = (i+1)%4;
-}
+    var windowXArray = [],
+        windowYArray = [];
+    
+    for (var i = 0; i < $(window).innerWidth(); i++) {
+        windowXArray.push(i);
+    }
+        
+    for (var i = 0; i < $(window).innerHeight(); i++) {
+        windowYArray.push(i);
+    }
+    
+    function randomPlacement(array) {
+        var placement = array[Math.floor(Math.random()*array.length)];
+        return placement;
+    }
+        
+    
+    var canvas = oCanvas.create({
+       canvas: '#canvas',
+       background: '#1c9e70',
+       fps: 60
+    });
+    
+    setInterval(function(){
+    
+    var rectangle = canvas.display.ellipse({
+       x: randomPlacement(windowXArray),
+       y: randomPlacement(windowYArray),
+       origin: { x: 'center', y: 'center' },
+       radius: 0,
+       fill: '#17ce40',
+       opacity: 0.8
+    });
+    
+    canvas.addChild(rectangle);
+    
+    rectangle.animate({
+      radius: 60,
+      opacity: 0
+    }, {
+      duration: '3000',
+      easing: 'linear',
+      callback: function () {
+                this.remove();
+            }
+    });
+    
+    }, 1000);
+    
+    $(window).resize(function(){
+    canvas.width = $(window).innerWidth();
+    canvas.height = $(window).innerHeight();
+    });
+    
+    $(window).resize();
+    
+    });
